e7e88246b19ab6e5e05c650a6e75fc3362a5df12
Dmitrij <dimaparfenyk1990@gmail.com>
2025-02-28 16:26:22 +0200

    create URL var

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,12 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
+const BASE_URL = "https://67c096a0b9d02a9f224a588d.mockapi.io/api/tasks";
+
 export const fetchTasks = createAsyncThunk(
   "tasks/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const res = await fetch(
-        "https://67c096a0b9d02a9f224a588d.mockapi.io/api/tasks"
-      );
+      const res = await fetch(`${BASE_URL}`);
       return await res.json();
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
@@ -18,16 +18,13 @@ export const addTask = createAsyncThunk(
   "tasks/addTask",
   async (text, thunkAPI) => {
     try {
-      const res = await fetch(
-        "https://67c096a0b9d02a9f224a588d.mockapi.io/api/tasks",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ text }),
-        }
-      );
+      const res = await fetch(`${BASE_URL}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text }),
+      });
       return await res.json();
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
@@ -39,15 +36,12 @@ export const deleteTask = createAsyncThunk(
   "tasks/deleteTask",
   async (id, thunkAPI) => {
     try {
-      const res = await fetch(
-        `https://67c096a0b9d02a9f224a588d.mockapi.io/api/tasks/${id}`,
-        {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      const res = await fetch(`${BASE_URL}/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
       return await res.json();
     } catch (e) {
@@ -60,14 +54,11 @@ export const toggleTask = createAsyncThunk(
   "tasks/toggleTask",
   async (task, thunkAPI) => {
     try {
-      const res = await fetch(
-        `https://67c096a0b9d02a9f224a588d.mockapi.io/api/tasks/${task.id}`,
-        {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ completed: !task.completed }),
-        }
-      );
+      const res = await fetch(`${BASE_URL}/${task.id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ completed: !task.completed }),
+      });
       return await res.json();
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
